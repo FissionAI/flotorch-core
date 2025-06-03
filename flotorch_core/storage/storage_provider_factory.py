e1d729eb30a14355889049ff54bfda8e03853283
shivatantarapelly-fl <shiva.tantarapelly@fissionlabs.com>
2025-06-03 10:23:01 +0530

    added storage provider by url changes

--- flotorch_core/storage/storage_provider_factory.py
+++ flotorch_core/storage/storage_provider_factory.py
@@ -2,6 +2,8 @@ from urllib.parse import urlparse
 from flotorch_core.storage.storage import StorageProvider
 from flotorch_core.storage.s3_storage import S3StorageProvider
 from flotorch_core.storage.local_storage import LocalStorageProvider
+from flotorch_core.storage.url_storage import UrlStorageProvider
+
 
 class StorageProviderFactory:
     """
@@ -13,6 +15,8 @@ class StorageProviderFactory:
         if parsed.scheme == "s3":
             bucket = parsed.netloc
             return S3StorageProvider(bucket)
+        elif parsed.scheme in ["http", "https"]:
+            return UrlStorageProvider()
         elif parsed.scheme == "" or parsed.scheme == "file":
             return LocalStorageProvider()
         else:
