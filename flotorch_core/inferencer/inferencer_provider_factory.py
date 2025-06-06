550b05abb922072d461c56b08fb816088c8301c7
DESKTOP-C010PSV\FL_LPT-470 <adil.raza@fissionlabs.com>
2025-06-06 19:41:23 +0530

    header support in gateway inferencer

--- flotorch_core/inferencer/inferencer_provider_factory.py
+++ flotorch_core/inferencer/inferencer_provider_factory.py
@@ -13,14 +13,15 @@ class InferencerProviderFactory:
     Factory to create inferencer based on the service name.
     """
     @staticmethod
-    def create_inferencer_provider(gateway_enabled: bool, base_url: str, api_key: str, service: str, model_id: str, region: str, arn_role: str, n_shot_prompts: int = 0, temperature: float = 0.7, n_shot_prompt_guide_obj: Dict[str, List[Dict[str, str]]] = None) -> BaseInferencer:
+    def create_inferencer_provider(gateway_enabled: bool, base_url: str, api_key: str, service: str, model_id: str, region: str, arn_role: str, n_shot_prompts: int = 0, temperature: float = 0.7, n_shot_prompt_guide_obj: Dict[str, List[Dict[str, str]]] = None, headers: Dict[str, str] = None) -> BaseInferencer:
         if gateway_enabled:
             return GatewayInferencer(
                 model_id=model_id, 
                 api_key=api_key, 
                 base_url=base_url, 
                 n_shot_prompts=n_shot_prompts, 
-                n_shot_prompt_guide_obj=n_shot_prompt_guide_obj
+                n_shot_prompt_guide_obj=n_shot_prompt_guide_obj,
+                headers=headers
             )
         
         if service == 'bedrock':
