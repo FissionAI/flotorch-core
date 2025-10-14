985e29139c4649cd191e5d973eb8151609eee668
Kiran George <kiran.george@fissionlabs.com>
2025-10-14 17:53:27 +0530

    gateway parameter fix

--- flotorch_core/inferencer/inferencer_provider_factory.py
+++ flotorch_core/inferencer/inferencer_provider_factory.py
@@ -16,14 +16,12 @@ class InferencerProviderFactory:
     def create_inferencer_provider(gateway_enabled: bool, base_url: str, api_key: str, service: str, model_id: str, region: str, arn_role: str, n_shot_prompts: int = 0, temperature: float = 0.7, n_shot_prompt_guide_obj: Dict[str, List[Dict[str, str]]] = None, headers: Dict[str, str] = None, max_tokens: int = None, topP: int = None) -> BaseInferencer:
         if gateway_enabled:
             return GatewayInferencer(
-                model_id=model_id, 
-                api_key=api_key, 
-                base_url=base_url, 
-                n_shot_prompts=n_shot_prompts, 
+                model_id=model_id,
+                api_key=api_key,
+                base_url=base_url,
+                n_shot_prompts=n_shot_prompts,
                 n_shot_prompt_guide_obj=n_shot_prompt_guide_obj,
-                headers=headers,
-                max_tokens=max_tokens, 
-                topP=topP
+                headers=headers
             )
         
         if service == 'bedrock':
